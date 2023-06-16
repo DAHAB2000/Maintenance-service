961a2738568de3b4057d159b5a50503c0f50a492
DAHAB2000 <dahabrostom3@gmail.com>
2023-06-16 15:32:17 +0300

    test

---
+++ js/about-data.js
@@ -0,0 +1,57 @@
+async function getDataTeam() {
+    let data = await fetch("https://abdelkader-mohamed.github.io/my-data-/clean-work/clean-work.json")
+    data = await data.json()
+    data.team.forEach(card => {
+      document.getElementById("team-data").innerHTML += `
+      <div class="col-lg-6">
+      <div class="row">
+        <div class="col-md-6 mb-5 mb-md-0">
+          <div><img src="${card.img}" class="img-fluid cardImg not" alt=""></div>
+        </div>
+        <div class="col-md-6 d-flex align-self-stretch mb-5 mb-md-0">
+          <div class="mainCard our-team mainCard--light">
+            <h4 class="mb-3" >${card.name}</h4>
+            <p>${card.content}</p>
+            <p class="pt-3 mb-0 border-top" ><a href="tel:+01118129509"><i class="bi bi-whatsapp me-2"></i>${card.PhoneNumber}</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+    });
+  }
+  getDataTeam();
+  async function getDataAbout() {
+    let data = await fetch("https://abdelkader-mohamed.github.io/my-data-/clean-work/clean-work.json")
+    data = await data.json();
+    data = data.about
+      document.getElementById("about-data").innerHTML = `
+      <div class="row align-items-center">
+      <div class="col-lg-6">
+        <div class="border-radius" >
+        <img src="${data.img}" class="img-fluid" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="p-3" >
+          <h2 class="mb-4 mt-3" >${ data.header}</h2>
+          <p> ${data.content1} <a href="#">${data.linkHash}</a>.</p>
+          <p class="mb-4" >${data.content2}</p>
+          <a href="contact.html" class="btn main-btn main-btn--scroll me-3 scrollTo" data-target="service">
+            <span >Get in touch</span>
+            <div class="marquee" aria-hidden="true">
+              <div class="marquee__inner" >
+                <span >Get in touch</span>
+                <span >Get in touch</span>
+                <span >Get in touch</span>
+                <span >Get in touch</span>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      </div>
+      `
+  }
+  getDataAbout();
+  
---
+++ js/offerOnly-data.js
@@ -0,0 +1,85 @@
+async function getData() {
+    let data = await fetch("https://abdelkader-mohamed.github.io/my-data-/clean-work/clean-work.json");
+    data = await data.json();
+    data = data.card;
+        document.getElementById("office-data").innerHTML += `
+        <div class="col-lg-6">
+        <div class="position-relative rounded-4 overflow-hidden " >
+          <a href="services-detail.html" class="position-relative">
+            <img src="${data.firstImg}" class="img-fluid cardImg services " alt="">
+            <img src="${data.secondImg}" class="img-fluid cardImg services cardImgHover" alt="">
+          </a>
+          <div class="second-bg d-flex justify-content-between position-absolute bottom-0 w-100" >
+            <p class="text-white mb-0 p-2" ><i class="bi bi-cash me-2"></i>$${data.salary}</p>
+            <p class="text-white mb-0 p-2" > <i class="bi bi-clock-fill me-2"></i> ${data.hours} hrs</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="p-4">
+          <h2 class="mb-4" >${data.cardTitle1} </h2>
+          <p class="mb-4" >${data.cardContent1} <a href="#">${data.linkHash}</a></p>
+          <h5>${data.cardTitle2}</h5>
+          <p>${data.cardContent2}</p>
+          <h5>${data.cardTitle3}</h5>
+          <p>${data.cardContent3}</p>
+        </div>
+      </div>
+        `
+  }
+  getData()
+  
+  async function getDataCards() {
+    let data = await fetch("https://abdelkader-mohamed.github.io/my-data-/clean-work/clean-work.json");
+    data = await data.json()
+        data.offers.forEach(card => {
+          if (card.case) {
+            document.getElementById("related-data").innerHTML += `
+            <div class="col-lg-6">
+    <div class="mainCard mainCard--light">
+    <div class="row align-items-center">
+        <div class="col-md-5 col-lg-12 col-xl-5 mb-4 mb-lg-4 mb-md-0 mb-xl-0 ps-lg-0">
+        <div class="position-relative rounded-4 overflow-hidden " >
+            <a href="services-detail.html" class="position-relative">
+              <img src="${card.firstImg}" class="img-fluid cardImg " alt="">
+              <img src="${card.secondImg}" class="img-fluid cardImg cardImgHover" alt="">
+            </a>
+            <div class="second-bg d-flex justify-content-between position-absolute bottom-0 w-100" >
+              <p class="text-white mb-0 p-2" ><i class="bi bi-cash me-2"></i> $${card.salary}</p>
+              <p class="text-white mb-0 p-2" > <i class="bi bi-clock-fill me-2"></i> ${card.hours} hrs</p>
+              </div>
+          </div>
+          </div>
+        <div class="col-md-7 col-lg-12 col-xl-7">
+          <h4><a href="services-detail.html">${card.cardTitle}</a></h4>
+          <p>${card.cardContent}</p>
+          <div class="mt-4 d-flex align-items-center justify-content-between">
+            <ul class="list-stars custom-list gap-1">
+              <li><i class="${card.star1}"></i></li>
+              <li><i class="${card.star2}"></i></li>
+              <li><i class="${card.star3}"></i></li>
+              <li><i class="${card.star4}"></i></li>
+              <li><i class="${card.star5}"></i></li>
+              </ul>
+            <a href="services-detail.html" class="btn main-btn main-btn--scroll">
+              <span >Learn More</span>
+              <div class="marquee" aria-hidden="true">
+                <div class="marquee__inner" >
+                  <span >Learn More</span>
+                  <span >Learn More</span>
+                  <span >Learn More</span>
+                  <span >Learn More</span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        `;
+      }
+      });
+  }
+  getDataCards();
+  
